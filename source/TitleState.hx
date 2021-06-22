4091e14481c8b2dd0fe3016c1029c6d9c03ce904
NerdiestAlpaca <70081038+NerdiestAlpaca@users.noreply.github.com>
2021-06-22 13:53:06 -0400

    Add files via upload

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -154,7 +154,7 @@ class TitleState extends MusicBeatState
 			FlxG.sound.music.fadeIn(4, 0, 0.7);
 		}
 
-		Conductor.changeBPM(102);
+		Conductor.changeBPM(95);
 		persistentUpdate = true;
 
 		var bg:FlxSprite = new FlxSprite().makeGraphic(FlxG.width, FlxG.height, FlxColor.BLACK);
