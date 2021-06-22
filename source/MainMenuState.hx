cbe1912ec3b1ad9acb55223be54cd321f4db13ba
NerdiestAlpaca <70081038+NerdiestAlpaca@users.noreply.github.com>
2021-06-22 13:42:19 -0400

    ???

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -89,7 +89,7 @@ class MainMenuState extends MusicBeatState
 
 		for (i in 0...optionShit.length)
 		{
-			var menuItem:FlxSprite = new FlxSprite(-100 + (i * 200), 600 );
+			var menuItem:FlxSprite = new FlxSprite((-100 + (i * 250)), 600 );
 			menuItem.frames = tex;
 			menuItem.animation.addByPrefix('idle', optionShit[i] + " basic", 24);
 			menuItem.animation.addByPrefix('selected', optionShit[i] + " white", 24);
