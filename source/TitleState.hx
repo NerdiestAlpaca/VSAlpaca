165ad673ffec875941f2d666f9a0571f74be153f
NerdiestAlpaca <70081038+NerdiestAlpaca@users.noreply.github.com>
2021-07-01 11:34:56 -0400

    Add files via upload

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -180,7 +180,7 @@ class TitleState extends MusicBeatState
 		gfDance.frames = Paths.getSparrowAtlas('gfDanceTitle');
 		gfDance.animation.addByIndices('danceLeft', 'gfDance', [30, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14], "", 24, false);
 		gfDance.animation.addByIndices('danceRight', 'gfDance', [15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29], "", 24, false);
-        animation.addByPrefix('cheer', 'gfEnter', 24, false);
+        gfDance.animation.addByPrefix('cheer', 'gfEnter', 24, false);
 		gfDance.antialiasing = true;
 		add(bgGrad);
 		add(gfDance);
