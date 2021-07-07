9616ad29a73411e3edfc55197344e22ea152d957
NerdiestAlpaca <70081038+NerdiestAlpaca@users.noreply.github.com>
2021-07-07 18:14:31 -0400

    Update PauseSubState.hx

--- source/PauseSubState.hx
+++ source/PauseSubState.hx
@@ -54,7 +54,7 @@ class PauseSubState extends MusicBeatSubstate
 		var levelDifficulty:FlxText = new FlxText(20, 15 + 32, 0, "", 32);
 		levelDifficulty.text += CoolUtil.difficultyString();
 		levelDifficulty.scrollFactor.set();
-		levelDifficulty.setFormat(Paths.font('vcr.ttf'), 32);
+		levelDifficulty.setFormat(Paths.font('pixel.otf'), 32);
 		levelDifficulty.updateHitbox();
 		add(levelDifficulty);
 
