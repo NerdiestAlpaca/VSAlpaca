f63e5de7aad023aeb2ff7e9a5398d4250ec77c92
NerdiestAlpaca <zoetrent4smash@gmail.com>
2021-07-08 17:20:03 -0400

    the fuckin uh
    file deletion

--- source/StoryMenuState.hx
+++ source/StoryMenuState.hx
@@ -25,12 +25,6 @@ class StoryMenuState extends MusicBeatState
 
 	var weekData:Array<Dynamic> = [
 		['Tutorial'],
-		['Bopeebo', 'Fresh', 'Dad Battle'],
-		['Spookeez', 'South', "Monster"],
-		['Pico', 'Philly Nice', "Blammed"],
-		['Satin Panties', "High", "Milf"],
-		['Cocoa', 'Eggnog', 'Winter Horrorland'],
-		['Senpai', 'Roses', 'Thorns'],
 		['Whimsy', 'Cyclone', 'Darkness', 'Absolution']
 	];
 	var curDifficulty:Int = 1;
@@ -39,23 +33,11 @@ class StoryMenuState extends MusicBeatState
 
 	var weekCharacters:Array<Dynamic> = [
 		['', 'bf', 'gf'],
-		['dad', 'bf', 'gf'],
-		['spooky', 'bf', 'gf'],
-		['pico', 'bf', 'gf'],
-		['mom', 'bf', 'gf'],
-		['parents-christmas', 'bf', 'gf'],
-		['senpai', 'bf', 'gf'],
 		['alpaca', 'bf', 'gf']
 	];
 
 	var weekNames:Array<String> = [
-		"",
-		"Daddy Dearest",
-		"Spooky Month",
-		"PICO",
-		"MOMMY MUST MURDER",
-		"RED SNOW",
-		"Hating Simulator ft. Moawling",
+		"Tutorial",
 		"Fluff n' Tumble"
 	];
 
