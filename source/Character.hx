f394e1f58ac3f9c8218935267bd45faa9cc74211
NerdiestAlpaca <zoetrent4smash@gmail.com>
2021-07-09 08:27:34 -0400

    sus

--- source/Character.hx
+++ source/Character.hx
@@ -493,7 +493,7 @@ class Character extends FlxSprite
 			case 'bf-car':
 				var tex = Paths.getSparrowAtlas('characters/bfCar');
 				frames = tex;
-				animation.addByPrefix('idle', 'BF idle dance', 24, false);
+				animation.addByPrefix('idle', 'BF idle dance', 24, true);
 				animation.addByPrefix('singUP', 'BF NOTE UP0', 24, false);
 				animation.addByPrefix('singLEFT', 'BF NOTE LEFT0', 24, false);
 				animation.addByPrefix('singRIGHT', 'BF NOTE RIGHT0', 24, false);
